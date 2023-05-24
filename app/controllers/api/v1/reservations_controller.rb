d67323a9ff92ec0e1982af8e2a7b861aa9a499b2
Brian Ludidi <bludidi5@gmail.com>
2023-05-24 17:11:40 +0200

    Add create action in the reservations controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -7,4 +7,27 @@ class Api::V1::ReservationsController < ApplicationController
         render json: @reservations, status: :ok
     end
 
+      def create
+        reservation = Reservation.new(reserve_params)
+        if booking.save!
+            render json: {
+              status: 201,
+              message: 'Car has been successfully booked',
+            }, status: :created
+        else
+            render json: { error: booking.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy 
+        @reservation.destroy
+        if booking.destroy
+            render json: {
+            status: 200,
+            message: 'Booking successfully canceled',
+            }, status: :ok
+          else
+            render json: { error: ' Unable to cancel the booking' }, status: :unprocessable_entity
+        end
+    end
 end
