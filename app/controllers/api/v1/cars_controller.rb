7ef166f222114d13af7242a13e7be39301b61b37
Brian Ludidi <bludidi5@gmail.com>
2023-05-24 16:51:45 +0200

    Create car controller index

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::CarsController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :destroy]
+    before_action :set_car, only: [:destroy, :show]
+
+    def index
+        @cars = Car.all
+        render json: {car: @cars}, status: :ok
+    end
+
 end
