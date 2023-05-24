3023189554af49a904b1eba259857388252a196e
tarikbouari <tarikbouari@gmail.com>
2023-05-24 15:24:35 +0000

    Fix linters

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,42 +1,42 @@
 class Api::V1::ReservationsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :destroy]
-    before_action :set_reservation, only: [:index, :destroy]
+  before_action :authenticate_user!, only: %i[create destroy]
+  before_action :set_reservation, only: %i[index destroy]
 
-    def index
-        @reservations = current_user.reservation.include(:cars).order(id: :desc)
-        render json: @reservations, status: :ok
-    end
+  def index
+    @reservations = current_user.reservation.include(:cars).order(id: :desc)
+    render json: @reservations, status: :ok
+  end
 
-      def create
-        reservation = Reservation.new(reserve_params)
-        if booking.save!
-            render json: {
-              status: 201,
-              message: 'Car has been successfully booked',
-            }, status: :created
-        else
-            render json: { error: booking.errors.full_messages }, status: :unprocessable_entity
-        end
+  def create
+    reservation = Reservation.new(reserve_params)
+    if reservation.save!
+      render json: {
+        status: 201,
+        message: 'Car has been successfully booked'
+      }, status: :created
+    else
+      render json: { error: reservation.errors.full_messages }, status: :unprocessable_entity
     end
+  end
 
-    def destroy 
-        @reservation.destroy
-        if booking.destroy
-            render json: {
-            status: 200,
-            message: 'Booking successfully canceled',
-            }, status: :ok
-          else
-            render json: { error: ' Unable to cancel the booking' }, status: :unprocessable_entity
-        end
+  def destroy
+    @reservation.destroy
+    if reservation.destroy
+      render json: {
+        status: 200,
+        message: 'reservation successfully canceled'
+      }, status: :ok
+    else
+      render json: { error: ' Unable to cancel the reservation' }, status: :unprocessable_entity
     end
-  
-  def set_reservation 
-        @reservation = Reservation.find(params[:id])
-    end
+  end
 
-    def reserve_params
-        params.require(:reservation).permit(:picking_date, :return_date, :car_id)
-        .merge(user_id: current_user.id)
-    end
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def reserve_params
+    params.require(:reservation).permit(:picking_date, :return_date, :car_id)
+      .merge(user_id: current_user.id)
+  end
 end
