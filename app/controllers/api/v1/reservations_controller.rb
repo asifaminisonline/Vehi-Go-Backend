0c6456ccb58c0b297632068431e2a160a78ee062
Brian Ludidi <bludidi5@gmail.com>
2023-05-24 17:10:28 +0200

    Create reservations_controller.rb

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::ReservationsController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy]
+    before_action :set_reservation, only: [:index, :destroy]
+
+    def index
+        @reservations = current_user.reservation.include(:cars).order(id: :desc)
+        render json: @reservations, status: :ok
+    end
+
 end
