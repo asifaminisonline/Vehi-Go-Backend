a26d8c8016102cf032b52a39f6dd06d2b2c25800
Brian Ludidi <bludidi5@gmail.com>
2023-05-24 16:52:34 +0200

    Create show in cars_controller.rb

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -7,4 +7,19 @@ class Api::V1::CarsController < ApplicationController
         render json: {car: @cars}, status: :ok
     end
 
+    def show
+        render json: {car: @car}, status: :ok
+    end
+    
+    def create 
+        car = Car.new(car_params)
+        if car.save!
+            render json: {
+              status: 201,
+              message: 'Car has been successfully created',
+            }, status: :created
+          else
+            render json: { error: 'ERROR: Unable to create the car' }, status: :unprocessable_entity
+          end
+    end
 end
