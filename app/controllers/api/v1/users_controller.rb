b63b7261efe67d65f39526505b676b005f81f63d
tarikbouari <tarikbouari@gmail.com>
2023-05-26 19:37:32 +0000

    Refactor code

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,50 @@
+class Api::V1::UsersController < ApplicationController
+  # skip_before_action :authenticate_request, only: [:create]
+  before_action :set_user, only: %i[destroy]
+  before_action :authenticate_user!
+
+  # Get /users
+  def index
+    @users = User.all
+    render json: current_user, status: :ok
+  end
+
+  # GET /users/{username}
+  def show
+    render json: { user: @user }, status: :ok
+  end
+
+  def new
+    @user = User.new
+  end
+
+  # create a new user  POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @users.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # Delete a user
+  def destroy
+    @user.destroy
+  end
+
+  # Update a user
+  def update
+    return if @user.update(user_params)
+
+    render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
