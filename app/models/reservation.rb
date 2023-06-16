4ec7738380453018aceb8218cee8ee5a6ed56d41
tarikbouari <tarikbouari@gmail.com>
2023-06-16 17:04:45 +0000

    Update code

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,8 @@
 class Reservation < ApplicationRecord
+  # validates :user_id, presence: true, uniqueness: true
+  # validates :car_id, presence: true, uniqueness: true
   validates :picking_date, presence: true
-  validates :return_date, presence: true
+  # validates :return_date, presence: true
 
   belongs_to :user, class_name: 'User'
   belongs_to :car, class_name: 'Car'
