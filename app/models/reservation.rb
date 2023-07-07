017ad6a24c6c7f03284ea875dccad6346a81b249
asifaminisonline <asifaminisonline@gmail.com>
2023-07-07 14:30:16 +0530

    Add new file

--- app/models/reservation.rb
+++
@@ -1,6 +0,0 @@
-class Reservation < ApplicationRecord
-  validates :picking_date, presence: true
-
-  belongs_to :user, class_name: 'User'
-  belongs_to :car, class_name: 'Car'
-end
--- db/migrate/20230516103030_add_default_to_cars.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultToCars < ActiveRecord::Migration[7.0]
-  def change
-    change_column_default :cars, :available, true
-  end
-end
